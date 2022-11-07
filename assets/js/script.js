a10fcc6bc6e4a04103c43f678efc07dfee0b04bc
Aletoria Booker <abooker77@yahoo.com>
2022-11-07 08:17:36 -0600

    fixed timestamp issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,64 +5,56 @@ function getApi(e) {
     e.preventDefault();
     
     // Create local variables
-    var city    = $('#city').val();
+    var city    = $('#city');
     var details = $('.city-details');
     var fiveDay = $('.five-day-cards');
     var api     = 'a34fcc159752966bf9fcfe3de164b68e';
 
-    // Fetch request
-    var requestCity = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=' + api; 
+    // Fetch request URL
+    var requestCity = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city.val() + '&limit=1&units=imperial&appid=' + api;
 
    fetch(requestCity)
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
-        console.log(data);
-        console.log('---------------------------');
-        console.log(data.list);
-        console.log('---------------------------');
-        
+        var list   = data.list;
         var myCity =  data.city.name;
         var degree = ' &#8457;';
+        var sDate; 
 
-        for (let i=0; i < data.list.length; i++){
-          var date =  moment(data.list[i].dt_txt).format('M/D/YYYY');
-          var temp = $('<div>').html('Temp: ' + data.list[i].main.temp + degree);
-          var wind = $('<div>').html('Wind: ' + data.list[i].wind.speed + ' MPH');
-          var humid = $('<div>').html('Humidity: ' + data.list[i].main.humidity + ' %');
-          
-          for (let j=0; j < data.list[i].weather.length; j++) {            
-            var icon = '<img src="http://openweathermap.org/img/wn/' + data.list[i].weather[j].icon + '.png">';
+        for (let i=0; i < list.length; i++) {
+          var date  = list[i].dt_txt; 
+          var nDate = moment(date).format('M/D/YYYY');
+          var temp  = $('<div>').html('Temp: ' + list[i].main.temp + degree);
+          var wind  = $('<div>').html('Wind: ' + list[i].wind.speed + ' MPH');
+          var humid = $('<div>').html('Humidity: ' + list[i].main.humidity + ' %'); 
+
+          // I just need ONE DATE out of 5 timestamps!!!
+          sDate = date.indexOf('12:00:00') > -1;
+
+          for (let j=0; j < list[i].weather.length; j++) {            
+            var icon = '<img src="http://openweathermap.org/img/wn/' + list[i].weather[j].icon + '.png">';
           }
 
-          //console.log(date + ',' + icon + ',' + temp + ',' + wind + ',' + humid);
-          var cityHead = $('<div>').addClass('city-name').html('<h3>' + myCity + ' (' + date + ') ' + icon + '</h3>');
-          var card     = $('<div>').addClass('col-lg-2 col-md-6');
+          var cityHead = $('<div>').addClass('city-name').html('<h3>' + myCity + ' (' + nDate + ') ' + icon + '</h3>');
 
+          // Output data
           if (i === 0) {
-            details
-              .append(cityHead)
-              .append(temp)
-              .append(wind)
-              .append(humid);
-          } else if (i > 0 && i <= 5 ) {
-              card
-                .append(date)
-                .append(icon)
-                .append(temp)
-                .append(wind)
-                .append(humid);
-
-                // Add 5 day cards to container
-                fiveDay.append(card);
+            details.append(cityHead).append(temp).append(wind).append(humid);
+          } else if (sDate) {
+            var card = $('<div>').addClass('col-lg-2 col-md-6');              
+            card.append(nDate).append(icon).append(temp).append(wind).append(humid);
           }
+          
+          // Add 5 day cards to container
+          fiveDay.append(card);
         }
       });
 
       // Refresh form
       form.reset();
-      $('#city').focus();
+      city.focus();
 
       return;
 }
